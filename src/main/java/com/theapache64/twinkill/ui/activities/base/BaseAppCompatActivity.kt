f44d80d0d52406de0da68c597611465fc2baa0f5
theapache64 <theapache64@gmail.com>
2019-03-13 18:33:38 +0530

    🌟 Added confirm dialog to BaseAppCompatActivity

--- src/main/java/com/theapache64/twinkill/ui/activities/base/BaseAppCompatActivity.kt
+++ src/main/java/com/theapache64/twinkill/ui/activities/base/BaseAppCompatActivity.kt
@@ -2,7 +2,11 @@ package com.theapache64.twinkill.ui.activities.base
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.theapache64.twinkill.R
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 @SuppressLint("Registered")
@@ -20,5 +24,20 @@ open class BaseAppCompatActivity : AppCompatActivity() {
     override fun onBackPressed() {
         supportFinishAfterTransition()
         super.onBackPressed()
+    }
+
+    /**
+     * Returns a confirm dialog with two button 'YES' and 'CANCEL'.
+     */
+    fun getConfirmDialog(@StringRes title: Int, @StringRes message: Int, onYes: () -> Unit): AlertDialog {
+        return AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(R.string.action_yes) { _: DialogInterface, _: Int ->
+                onYes()
+            }
+            .setCancelable(false)
+            .setNegativeButton(android.R.string.cancel, null)
+            .create()
     }
 }
