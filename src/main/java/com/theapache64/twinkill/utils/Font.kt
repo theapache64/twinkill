aabb0a994174b2e729aa5699196bdec9c62cfe58
theapache64 <theapache64@gmail.com>
2019-04-01 20:19:13 +0530

    🌄 Separated font module

--- src/main/java/com/theapache64/twinkill/utils/Font.kt
+++ src/main/java/com/theapache64/twinkill/utils/Font.kt
@@ -1,7 +1,3 @@
 package com.theapache64.twinkill.utils
 
-enum class Font(val path: String) {
-    GoogleSansRegular("fonts/GoogleSans-Regular.ttf"),
-    GoogleSansMedium("fonts/GoogleSans-Medium.ttf"),
-    GoogleSansBold("fonts/GoogleSans-Bold.ttf"),
-}
+class Font(val path: String)
