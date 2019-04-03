84402a0b31feea3c198602cd789c385875a4c1b6
theapache64 <theapache64@gmail.com>
2019-04-03 19:45:02 +0530

    ◀️ backup

---
+++ google_fonts/src/main/java/com/theapache64/twinkill/googlefonts/GoogleFonts.kt
@@ -0,0 +1,9 @@
+package com.theapache64.twinkill.googlefonts
+
+import com.theapache64.twinkill.utils.Font
+
+object GoogleFonts {
+    val GoogleSansRegular = Font("fonts/GoogleSans-Regular.ttf")
+    val GoogleSansMedium = Font("fonts/GoogleSans-Medium.ttf")
+    val GoogleSansBold = Font("fonts/GoogleSans-Bold.ttf")
+}
