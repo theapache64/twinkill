92b3a0e04f1f7ee7cdb7d0de7591677989ab25f7
theapache64 <theapache64@gmail.com>
2019-12-12 15:20:23 +0530

    ⬆️ upgrade loadingview

--- core/src/main/java/com/theapache64/twinkill/ui/widgets/LoadingView.kt
+++ core/src/main/java/com/theapache64/twinkill/ui/widgets/LoadingView.kt
@@ -11,8 +11,10 @@ import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import com.theapache64.twinkill.databinding.LoadingViewBinding
 
+@Suppress("MemberVisibilityCanBePrivate")
 class LoadingView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
+    private var hasRetryCallback: Boolean = false
     private val binding: LoadingViewBinding
 
     init {
@@ -23,8 +25,11 @@ class LoadingView(context: Context, attrs: AttributeSet?) : FrameLayout(context,
     /**
      * Setting retry callback
      */
-    fun setRetryCallback(retryCallback: RetryCallback) {
-        binding.retryCallback = retryCallback
+    fun setRetryCallback(retryCallback: () -> Unit) {
+        this.hasRetryCallback = true
+        binding.bRetry.setOnClickListener {
+            retryCallback()
+        }
     }
 
     /**
@@ -32,9 +37,15 @@ class LoadingView(context: Context, attrs: AttributeSet?) : FrameLayout(context,
      */
     fun showLoading(message: String) {
         visibility = View.VISIBLE
-        binding.isLoading = true
-        binding.isError = false
-        binding.message = message
+
+        // View visibility
+        binding.clpb.visibility = View.VISIBLE
+        binding.tvMessage.visibility = View.VISIBLE
+        binding.ivError.visibility = View.GONE
+        binding.bRetry.visibility = View.GONE
+
+        // Data change
+        binding.tvMessage.text = message
     }
 
     fun showLoading(@StringRes message: Int) {
@@ -43,16 +54,22 @@ class LoadingView(context: Context, attrs: AttributeSet?) : FrameLayout(context,
 
     fun hideLoading() {
         visibility = View.GONE
-        binding.isLoading = false
-        binding.isError = false
-        binding.message = null
     }
 
     fun showError(message: String) {
         visibility = View.VISIBLE
-        binding.isError = true
-        binding.isLoading = false
-        binding.message = message
+
+        // View visibility
+        binding.tvMessage.visibility = View.VISIBLE
+        binding.ivError.visibility = View.VISIBLE
+        binding.clpb.visibility = View.GONE
+        binding.bRetry.visibility = if (hasRetryCallback) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
+        binding.tvMessage.text = message
     }
 
     fun setTextColor(@ColorRes color: Int) {
@@ -61,9 +78,5 @@ class LoadingView(context: Context, attrs: AttributeSet?) : FrameLayout(context,
 
     fun setErrorRes(@DrawableRes drawable: Int) {
         binding.ivError.setImageResource(drawable)
-    }
-
-    interface RetryCallback {
-        fun onRetryClicked()
     }
 }
